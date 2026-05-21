c74d9910d17251c9f7a4a9b5db765b13d5828219
mariajosecruz <02230131003@mail.udes.edu.co>
2026-05-20 21:25:49 -0500

    initialize Spring Boot application with basic product management features

---
+++ src/main/java/com/universidad/productosservice/CruzzPost1U9Application.java
@@ -0,0 +1,13 @@
+package com.universidad.productosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruzzPost1U9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruzzPost1U9Application.class, args);
+    }
+
+}
